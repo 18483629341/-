0625d73ab3209ab9d16c2fee3563a78e24d772f5
18483629341 <714402934@qq.com>
2019-04-25 15:00:38 +0800

    left-rigth  ok

--- js/home.js
+++ js/home.js
@@ -1,11 +1,8 @@
 $(function () {
-    var cityLength=$('.level-data-li').length;
-    $('.level-data-list').css('width', $('.level-data-li').width()*cityLength+50*(cityLength-1)+100+'px');
-     /* 仿滚动条 */
-     $(".center-bottom").mCustomScrollbar({
-        //scrollButtons:{ enable:true } ,
-        axis: "x"           //"x","y",值为字符串，分别对应横纵向滚动
-     });
+    //各市区信息显示模块：鼠标滚动时水平移动/自动滚动/点击左箭头，左滚动/点击右箭头右滚动
+    selfCustomScroll(3);
+    
+   
     /* 外环的旋转*/
     var rollPanel1 = new RollPanel('#out-rotate-item-main');/*参数一：需要旋转的元素选择器 */
     /* 内环的旋转*/
@@ -20,63 +17,42 @@ $(function () {
     draw('canvas2', 9);
     /* 右边曲线上所有的光点的方法对象  */
     var lightLoopRight = new LightLoop('#lignt-box-right', 9, 'converage');
-
-    var status = 'converage';
-    $("body").on('click', '.data-collection', function () {
-        $(this).siblings('.tab-li').removeClass('active');
-        $(this).addClass('active');
-        lightLoopLeft.setType('converage');
-        lightLoopRight.setType('converage');
-        $(".down-icon").removeClass('up-side-down');
-            $(".up-icon").removeClass('up-side-down');
-    })
-    $("body").on('click', '.data-serve', function () {
-        $(this).siblings('.tab-li').removeClass('active');
-        $(this).addClass('active');
-        lightLoopLeft.setType("spread");
-        lightLoopRight.setType('spread');
-        $(".down-icon").addClass('up-side-down');
-        $(".up-icon").addClass('up-side-down');
-    })
-
+   
     //数据汇聚和数据服务的循环切换 使用setTimeout()模拟setInterval()，才能准确在间隔时间内执行方法
     var i = 0;
     var timer = setTimeout(function () {
+        //console.log(status);
         leftRightAlter();
-
         timer = setTimeout(arguments.callee, loopTime);
     }, loopTime)
     //当前的状态:数据汇聚对应的'converage'；数据服务对应的'service'；
     var status = 'converage';
     //统一循环控制集中和扩散的时间
-    var loopTime = 30000;
-    var animateTime = 29500;
+    var loopTime = 30000;//30000
+    var animateTime = 29500;//29500
+    var leftRightTimer=null;
+    var leftRightGapTime=loopTime/1582*19;
+    leftRightTimer=setInterval(function(){
+        arrowTran('converage');
+    },leftRightGapTime); 
+
 
     function leftRightAlter() {
         //数据汇聚和数据服务的方法
         if (parseInt(i % 2) === parseInt(0)) {
             //此处展示数据汇聚的内容
-
-            //光点向右移动
-            $('.rotate-radio').animate({ width: '100%' }, animateTime);
-            $('.light').animate({ left: '100%' }, animateTime, function () {
-                //状态转变到数据服务
-                spreadTranslate()//整体向外扩散
-                status = 'service';
-            });
+            spreadTranslate()//整体向外扩散 
+           
         } else if (parseInt(i % 2) === parseInt(1)) {
             //此处展示数据服务的内容
-
-            //光点向左移动
-            $('.rotate-radio').animate({ width: '0' }, animateTime);
-            $('.light').animate({ left: '0' }, animateTime, function () {
-                //状态转变到数据汇聚
-                collectTranslate();  //整体向中心集中
-                status = 'converage';
-            });
+            collectTranslate(); 
         }
+        //console.log(status);
         i++;
     }
+   
+   
+    
 
     //循环播放数据
     CenterValueLoop();//虚拟展示              
@@ -84,57 +60,72 @@ $(function () {
     //整体向中心集中
     function collectTranslate() {
         //console.log(1);
-        if (status != 'converage') {
-            $(".down-icon").removeClass('up-side-down');
-            $(".up-icon").removeClass('up-side-down');
-            lightLoopLeft.setType("converage");//converage 往集中方向
-            lightLoopRight.setType('converage');
+        if (status != 'service') {
+            status = 'service';
+            //$('span').remove('.arrow-icon');
+            clearInterval(leftRightTimer);
+            leftRightTimer=setInterval(function(){
+                arrowTran('service');
+            },leftRightGapTime);
+            $(".down-icon").addClass('up-side-down');
+            $(".up-icon").addClass('up-side-down');
+            lightLoopLeft.setType("spread");//converage 往扩散方向
+            lightLoopRight.setType('spread'); 
+            
+           
         }
+       
     }
 
     //整体向外扩散
     function spreadTranslate() {
-        if (status != 'service') {
-            $(".down-icon").addClass('up-side-down');
-            $(".up-icon").addClass('up-side-down');
-            lightLoopLeft.setType("spread");//converage 往扩散方向
-            lightLoopRight.setType('spread');
+        if (status != 'converage') {
+            status = 'converage'; 
+            clearInterval(leftRightTimer);
+            leftRightTimer=setInterval(function(){
+                
+                arrowTran('converage');
+            },leftRightGapTime);  
+            $(".down-icon").removeClass('up-side-down');
+            $(".up-icon").removeClass('up-side-down');
+            lightLoopLeft.setType("converage");//converage 往集中方向
+            lightLoopRight.setType('converage');
+                
         }
     }
-
-    // $("#left-point").hover(function() {
-    //     // $(this).stop(true)
-    //     $('.rotate-radio').stop(true).animate({ width: '0%' }, 500);     //如果在此时触发了光标的移出事件
-    //     //                                           //直接跳过后面的动画队列
-    //     $('.light').stop(true).animate({ left: '0' }, 500);
-    //     clearTimeout(timer);
-    // },function(){
-    //     timer = setTimeout(function () {
-    //         leftRightAlter();
-    //         timer = setTimeout(arguments.callee, loopTime);
-    //     }, loopTime)
-    // }).trigger("mouseleave");
-
-    // $("#right-point").hover(function() {
-    //     $('.rotate-radio').stop(true).animate({ width: '100%' }, 500);//如果在此时触发了光标的移出事件
-    //     $('.light').stop(true).animate({ left: '100%' }, 500);//直接跳过后面的动画队列
-    // });
-    // $("body").on('click', '#left-point', function () {
-    //     clearTimeout(timer);
-    //     //整体向中心集中
-    //     status = 'converage';
-    //     i=0;
-    //     timer = setTimeout(function () {
-    //         leftRightAlter();
-
-    //         timer = setTimeout(arguments.callee, loopTime);
-    //     }, loopTime)
-    // })
-
-    // $('body').on('click', '#right-point', function () {
-    //     // //整体向外扩散
-    //     status = 'service';
-    //     i=1;
-    // })
+    
+   
 })
 
+ //点击按钮-- 数据汇聚
+ $("body").on('click', '.data-collection', function () {
+    $(this).siblings('.tab-li').removeClass('active');
+    $(this).addClass('active');
+    lightLoopLeft.setType('converage');
+    lightLoopRight.setType('converage');
+    $(".down-icon").removeClass('up-side-down');
+        $(".up-icon").removeClass('up-side-down');
+})
+
+//点击按钮-- 数据服务
+$("body").on('click', '.data-serve', function () {
+    $(this).siblings('.tab-li').removeClass('active');
+    $(this).addClass('active');
+    lightLoopLeft.setType("spread");
+    lightLoopRight.setType('spread');
+    $(".down-icon").addClass('up-side-down');
+    $(".up-icon").addClass('up-side-down');
+})
+
+/**
+ * 水平移动的基本方法  
+ * @param {string} type  converage 或service
+ */
+function arrowTran(type) {
+   
+    if(type==='converage'){
+        $('.arrow-box').append('<span class="arrow-icon"></span>');
+    }else{
+        $('.arrow-icon').eq(-1).remove();
+    } 
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var swidth = null;//获取屏幕的宽度
+var swidth = null; //获取屏幕的宽度
 function autoFit() {
     swidth = $(window).width();
     if (swidth > 1000 || swidth === 1000) {
@@ -12,79 +12,79 @@ function resize() {
     $('.body1').css({
         transform: "scale(" + winratio + ")",
         transformOrigin: "left top",
-        height:winratio*1080+'px'
+        height: winratio * 1080 + 'px'
     });
 }
 
-window.requestAnimFrame = (function() {
+window.requestAnimFrame = (function () {
     return window.requestAnimationFrame ||
-           window.webkitRequestAnimationFrame ||
-           window.mozRequestAnimationFrame  || 
-           window.oRequestAnimationFrame || 
-           window.msRequestAnimationFrame ||
-	       function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-			  return window.setTimeout(callback, 1000 / 60);
-		   };
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function ( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+            return window.setTimeout(callback, 1000 / 60);
+        };
 })();
 
 
 
 //利用canvas绘制曲线
 /**
-* 
-* @param {*} elementId 绘制曲线的canvas的ID
-* @param {*} n         需要绘制曲线的数量
-*/
+ * 
+ * @param {*} elementId 绘制曲线的canvas的ID
+ * @param {*} n         需要绘制曲线的数量
+ */
 function draw(elementId, n) {
     var canvas = document.getElementById(elementId);
     //var boxHeight=canvas.style.height;
     //设置或得到整个canvas的高度
-   
-    var boxHeight = $('#' + elementId).height();         //canvas的高度 待完善
-    var boxWidth = $('#' + elementId).width();          //canvas的宽度 待完善
+
+    var boxHeight = $('#' + elementId).height(); //canvas的高度 待完善
+    var boxWidth = $('#' + elementId).width(); //canvas的宽度 待完善
     var perHeight = parseInt(boxHeight / n);
     var centerY = parseInt(boxHeight / 2);
-   
+
     var context = canvas.getContext('2d');
 
     //绘制2次贝塞尔曲线 
-    context.setLineDash([6, 6]);//设置线条为虚线的样式
+    context.setLineDash([6, 6]); //设置线条为虚线的样式
     for (var i = 0; i < n; i++) {
         var startY = perHeight / 2 + perHeight * i;
-        var controlY=parseInt(centerY+perHeight/16*(i-n/2));
+        var controlY = parseInt(centerY + perHeight / 16 * (i - n / 2));
         context.beginPath();
 
         context.moveTo(0, startY); //曲线绘制的起点
         //quadraticCurveTo(cpx,cpy,x,y)　　//cpx，cpy表示控制点的坐标,x，y表示终点坐标；
         //曲线绘制的控制点位整个canvas
         context.quadraticCurveTo(30, controlY, boxWidth, centerY);
-        context.strokeStyle = "#078dff";//设置贝塞尔曲线的颜色
+        context.strokeStyle = "#078dff"; //设置贝塞尔曲线的颜色
         context.stroke();
     }
 }
 
 //移动点的原型
 /**
-* 
-* @param {*} element   移动点的父元素的选择器
-* @param {*} n         总共移动点的数量
-* @param {*} direction     需要绘制曲线的方向,只能为 "converage"|'spread'
-*/
-function LightLoop(element, n,type) {
-    this.element=element
-    this.type=type;
-    this.boxHeight = $(this.element).height();         //移动点的的高度  
-    this.boxWidth = $(this.element).width();          //移动点的父元素的的宽度
+ * 
+ * @param {*} element   移动点的父元素的选择器
+ * @param {*} n         总共移动点的数量
+ * @param {*} direction     需要绘制曲线的方向,只能为 "converage"|'spread'
+ */
+function LightLoop(element, n, type) {
+    this.element = element
+    this.type = type;
+    this.boxHeight = $(this.element).height(); //移动点的的高度  
+    this.boxWidth = $(this.element).width(); //移动点的父元素的的宽度
     this.perHeight = parseInt(this.boxHeight / n);
-    this.childWidth = $(this.element).children().width();                   //移动点的子元素的宽度    
-    this.childHeight = $(this.element).children().height();                 //移动点的子元素的高度
+    this.childWidth = $(this.element).children().width(); //移动点的子元素的宽度    
+    this.childHeight = $(this.element).children().height(); //移动点的子元素的高度
     //控制点p1统一为
-    this.controlX = 30;                                          //离canvas做左侧的水平距离 统一为30；
+    this.controlX = 30; //离canvas做左侧的水平距离 统一为30；
     //终点p2统一为右边终点
     this.endX = this.boxWidth;
-    this.endY = this.boxHeight / 2;//垂直中心
-    this.radio = 0;//贝塞尔曲线的比值
-    
+    this.endY = this.boxHeight / 2; //垂直中心
+    this.radio = 0; //贝塞尔曲线的比值
+
     var _this = this;
     var LightTurnOn = setInterval(function () {
         _this.turn();
@@ -98,35 +98,35 @@ function LightLoop(element, n,type) {
         }
         for (var i = 0; i < n; i++) {
             var obj = {};
-             //控制点p1
-            obj.controlY= this.boxHeight / 2+this.perHeight/16*(i-n/2);                          
+            //控制点p1
+            obj.controlY = this.boxHeight / 2 + this.perHeight / 16 * (i - n / 2);
             //如果是光点往集中方向移动的类型
-            if(this.type==="converage"){
-            //起点p0                
-                obj.startX = 0;                                              //离canvas做左侧的水平距离            
-                obj.startY = this.perHeight / 2 + this.perHeight * i;             //离canvas顶部的垂直距离  
-                obj.endX=this.endX;
-                obj.endY=this.endY;    
+            if (this.type === "converage") {
+                //起点p0                
+                obj.startX = 0; //离canvas做左侧的水平距离            
+                obj.startY = this.perHeight / 2 + this.perHeight * i; //离canvas顶部的垂直距离  
+                obj.endX = this.endX;
+                obj.endY = this.endY;
                 //计算斜率，得到点的切线方向，得到角度
                 obj.k = [(1 - this.radio) * (obj.controlY - obj.startY) + this.radio * (obj.endY - obj.controlY)] / [(1 - this.radio) * (this.controlX - obj.startX) + this.radio * (obj.endX - this.controlX)];
                 //根据斜率，求得需要切斜的角度 单位为弧度，/0.017453293 转化为角度
-                obj.angle = Math.atan(obj.k) / 0.017453293;//根据斜率得到旋转角度
-            //如果是光点往分散方向移动的类型
-            }else{
-                obj.startX = this.endX;                                                       
-                obj.startY = this.endY;             
-                obj.endX=0;
-                obj.endY=this.perHeight / 2 + this.perHeight * i;  
-        
+                obj.angle = Math.atan(obj.k) / 0.017453293; //根据斜率得到旋转角度
+                //如果是光点往分散方向移动的类型
+            } else {
+                obj.startX = this.endX;
+                obj.startY = this.endY;
+                obj.endX = 0;
+                obj.endY = this.perHeight / 2 + this.perHeight * i;
+
                 //计算斜率，得到点的切线方向，得到角度
                 obj.k = [(1 - this.radio) * (obj.controlY - obj.startY) + this.radio * (obj.endY - obj.controlY)] / [(1 - this.radio) * (this.controlX - obj.startX) + this.radio * (obj.endX - this.controlX)];
-               
+
                 //根据斜率，求得需要切斜的角度 单位为弧度，/0.017453293 转化为角度
-                obj.angle = Math.atan(obj.k) / 0.017453293+180; //根据斜率得到旋转角度，+180另外图标自身要换反方向
+                obj.angle = Math.atan(obj.k) / 0.017453293 + 180; //根据斜率得到旋转角度，+180另外图标自身要换反方向
             }
-             //根据比值this.radio变化计算点的坐标值；p=(1-this.radio)*(1-this.radio)p0+2*this.radio*(1-this.radio)*p1+this.radio*this.radio*p2;
-             obj.nowX = (1 - this.radio) * (1 - this.radio) * obj.startX + 2 * this.radio * (1 - this.radio) * this.controlX + this.radio * this.radio * obj.endX;
-             obj.nowY = (1 - this.radio) * (1 - this.radio) * obj.startY + 2 * this.radio * (1 - this.radio) *obj.controlY + this.radio * this.radio * obj.endY;
+            //根据比值this.radio变化计算点的坐标值；p=(1-this.radio)*(1-this.radio)p0+2*this.radio*(1-this.radio)*p1+this.radio*this.radio*p2;
+            obj.nowX = (1 - this.radio) * (1 - this.radio) * obj.startX + 2 * this.radio * (1 - this.radio) * this.controlX + this.radio * this.radio * obj.endX;
+            obj.nowY = (1 - this.radio) * (1 - this.radio) * obj.startY + 2 * this.radio * (1 - this.radio) * obj.controlY + this.radio * this.radio * obj.endY;
             $(this.element + ' .line-icon:eq(' + i + ')').css({
                 'left': obj.nowX - this.childWidth / 2,
                 'top': obj.nowY - this.childHeight / 2,
@@ -135,32 +135,63 @@ function LightLoop(element, n,type) {
         }
     }
 
-     //重置光点移动方式
-    this.setType=function(newType){                                 
-        this.type=newType;
+    //重置光点移动方式
+    this.setType = function (newType) {
+        this.type = newType;
         clearInterval(LightTurnOn);
-        this.radio = 0;//重置曲线的绘制比值
+        this.radio = 0; //重置曲线的绘制比值
         LightTurnOn = setInterval(function () {
             _this.turn();
         }, 40)
     }
-    
+
 }
 
 //循环播放中间球体数据
-function CenterValueLoop(){
-     var ListNumArr=['233,444','233,566','33,589'];
-     var KbNumArr=['789,233,444','677,233,566','556,533,589'];
-     var i=0;
-     //循环数据播放
-     setInterval(function(){
+function CenterValueLoop() {
+    var ListNumArr = ['233,444', '233,566', '33,589'];
+    var KbNumArr = ['789,233,444', '677,233,566', '556,533,589'];
+    var i = 0;
+    //循环数据播放
+    setInterval(function () {
         $('.list-num').html(ListNumArr[i]);
         $('.kb-num').html(KbNumArr[i]);
-        if(i>=ListNumArr.length){
-            i=0
-        }else{
+        if (i >= ListNumArr.length) {
+            i = 0
+        } else {
             i++;
         }
-     },500)
-     
+    }, 500)
+
 };
+
+
+/**
+ * 各市区信息显示模块（容器为.center-bottom）：鼠标滚动时水平移动/自动滚动/点击左箭头，左滚动/点击右箭头右滚动
+ * @param {Integer} n 点击按钮/鼠标滚动
+ */
+function selfCustomScroll(n) {
+    /* 仿滚动条 */
+    var cityLength = $('.city-data-li').length;
+    $('.city-data-list').css('width', $('.city-data-li').width() * cityLength + 50 * (cityLength - 1) + 100 + 'px');
+    var translateNum = n; //设置
+    $(".center-bottom").mCustomScrollbar({
+        axis: "x", //"x","y",值为字符串，分别对应横纵向滚动
+        //scrollInertia:0,
+        mouseWheel: {
+            //enable:true,
+            //scrollAmount:$('.city-data-li').width()*1+50*(1-1),
+            // preventDefault:true,
+            // invert:true
+        },
+        scrollButtons: {
+            enable: true,
+            // scrollType:"continuous",
+            scrollSpeed: 20,
+            scrollAmount: $('.city-data-li').width() * translateNum + 50 * (translateNum - 1)
+        },
+        //scrollbarPosition: "outside" ,  
+        //advanced:{ updateOnContentResize:true } ,
+    });
+}
+
